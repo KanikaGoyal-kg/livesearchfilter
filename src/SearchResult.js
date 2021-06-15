3ec5b3d4c4f81cd86466e484d75ba5f8a1ff86f4
Kanika Goyal <goyalkanu111@gmail.com>
2021-06-15 21:20:17 +0530

    Done with data of images of searchbar

---
+++ src/SearchResult.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SearchResult = () => {
+
+    const img = `https://source.unsplash.com/user/erondu/1600*900`;
+
+    return (
+        <>
+        <div>
+            <img src={img} alt="search" />
+        </div>
+        </>
+    )
+}
+
+export default SearchResult;
