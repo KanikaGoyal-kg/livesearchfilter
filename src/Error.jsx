67c4ed39230cf9dd1ae9cbb49e6ed513a89c7a70
Kanika Goyal <goyalkanu111@gmail.com>
2021-06-16 21:03:39 +0530

    done with 404 error

---
+++ src/Error.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Error = () => {
+
+    return (
+        <>
+        <div className="setStyle2">
+            <h1> 404 Error page </h1>
+            <p> Sorry, this page does not exist </p>
+        </div>
+        </>
+    )
+}
+
+export default Error;
